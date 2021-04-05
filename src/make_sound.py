8ccab7745805a0a15472a8643f2277df217ad250
Seb K <sebastian.kelly@reachplc.com>
2021-04-05 15:37:53 +0100

    _

--- src/make_sound.py
+++ src/make_sound.py
@@ -1,7 +1,7 @@
 import subprocess
 
 def hoot():
-  command = "omxplayer -o alsa:hw:1,0 ../aseets/owl_sound.mp3 --vol 200".split(' ')
+  command = "omxplayer -o alsa:hw:1,0 ../assets/owl_sound.mp3 --vol 200".split(' ')
   subprocess.Popen(command, 
                   stdin=subprocess.PIPE, 
                   stdout=subprocess.PIPE, 
