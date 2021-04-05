1926bec0dea5f473e2bf429793576e5bea3cf48c
Seb K <sebastian.kelly@reachplc.com>
2021-04-05 21:49:51 +0100

    _

--- picamera-h264-web-streaming/broadcast.py
+++ picamera-h264-web-streaming/broadcast.py
@@ -97,49 +97,49 @@ class BroadcastThread(Thread):
                     self.output.condition.wait()
                     frame = self.output.frame
                     print("FRAME: ", frame)
-                    cv2.CV_LOAD_IMAGE_COLOR = 1  # set flag to 1 to give colour image
-                    npframe = np.fromstring(frame, dtype=np.uint8)
-                    pil_frame = cv2.imdecode(npframe, cv2.CV_LOAD_IMAGE_COLOR)
-                    #pil_frame = cv2.imdecode(frame,-1)
-                    frame_rgb = cv2.cvtColor(pil_frame, cv2.COLOR_BGR2RGB)
-                    frame_resized = cv2.resize(frame_rgb, (width, height))
-                    input_data = np.expand_dims(frame_resized, axis=0)
+                    # cv2.CV_LOAD_IMAGE_COLOR = 1  # set flag to 1 to give colour image
+                    # npframe = np.fromstring(frame, dtype=np.uint8)
+                    # pil_frame = cv2.imdecode(npframe, cv2.CV_LOAD_IMAGE_COLOR)
+                    # #pil_frame = cv2.imdecode(frame,-1)
+                    # frame_rgb = cv2.cvtColor(pil_frame, cv2.COLOR_BGR2RGB)
+                    # frame_resized = cv2.resize(frame_rgb, (width, height))
+                    # input_data = np.expand_dims(frame_resized, axis=0)
 
-                    # Normalize pixel values if using a floating model (i.e. if model is non-quantized)
-                    if floating_model:
-                        input_data = (np.float32(input_data) - input_mean) / input_std
+                    # # Normalize pixel values if using a floating model (i.e. if model is non-quantized)
+                    # if floating_model:
+                    #     input_data = (np.float32(input_data) - input_mean) / input_std
 
-                    # Perform the actual detection by running the model with the image as input
-                    interpreter.set_tensor(input_details[0]['index'], input_data)
-                    interpreter.invoke()
+                    # # Perform the actual detection by running the model with the image as input
+                    # interpreter.set_tensor(input_details[0]['index'], input_data)
+                    # interpreter.invoke()
 
-                    boxes = interpreter.get_tensor(output_details[0]['index'])[0]
-                    classes = interpreter.get_tensor(output_details[1]['index'])[0]
-                    scores = interpreter.get_tensor(output_details[2]['index'])[0]
-                    for i in range(len(scores)):
-                        if ((scores[i] > min_conf_threshold) and (scores[i] <= 1.0)):
+                    # boxes = interpreter.get_tensor(output_details[0]['index'])[0]
+                    # classes = interpreter.get_tensor(output_details[1]['index'])[0]
+                    # scores = interpreter.get_tensor(output_details[2]['index'])[0]
+                    # for i in range(len(scores)):
+                    #     if ((scores[i] > min_conf_threshold) and (scores[i] <= 1.0)):
 
-                            ymin = int(max(1, (boxes[i][0] * imH)))
-                            xmin = int(max(1, (boxes[i][1] * imW)))
-                            ymax = int(min(imH, (boxes[i][2] * imH)))
-                            xmax = int(min(imW, (boxes[i][3] * imW)))
+                    #         ymin = int(max(1, (boxes[i][0] * imH)))
+                    #         xmin = int(max(1, (boxes[i][1] * imW)))
+                    #         ymax = int(min(imH, (boxes[i][2] * imH)))
+                    #         xmax = int(min(imW, (boxes[i][3] * imW)))
 
-                            cv2.rectangle(frame, (xmin, ymin), (xmax, ymax),
-                                        (10, 255, 0), 2)
+                    #         cv2.rectangle(frame, (xmin, ymin), (xmax, ymax),
+                    #                     (10, 255, 0), 2)
 
-                            object_name = labels[int(classes[i])]
-                            label = '%s: %d%%' % (object_name, int(scores[i] * 100)
-                                                )  # Example: 'person: 72%'
-                            labelSize, baseLine = cv2.getTextSize(label,
-                                                                cv2.FONT_HERSHEY_SIMPLEX,
-                                                                0.7, 2)
-                            label_ymin = max(ymin, labelSize[1] + 10)
-                            cv2.rectangle(
-                                frame, (xmin, label_ymin - labelSize[1] - 10),
-                                (xmin + labelSize[0], label_ymin + baseLine - 10),
-                                (255, 255, 255), cv2.FILLED)
-                            cv2.putText(frame, label, (xmin, label_ymin - 7),
-                                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2)
+                    #         object_name = labels[int(classes[i])]
+                    #         label = '%s: %d%%' % (object_name, int(scores[i] * 100)
+                    #                             )  # Example: 'person: 72%'
+                    #         labelSize, baseLine = cv2.getTextSize(label,
+                    #                                             cv2.FONT_HERSHEY_SIMPLEX,
+                    #                                             0.7, 2)
+                    #         label_ymin = max(ymin, labelSize[1] + 10)
+                    #         cv2.rectangle(
+                    #             frame, (xmin, label_ymin - labelSize[1] - 10),
+                    #             (xmin + labelSize[0], label_ymin + baseLine - 10),
+                    #             (255, 255, 255), cv2.FILLED)
+                    #         cv2.putText(frame, label, (xmin, label_ymin - 7),
+                    #                     cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2)
                     self.websocket_server.manager.broadcast(self.output.frame, binary=True)
                     
         except:
