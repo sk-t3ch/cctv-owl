9a9979b2f2e4816c0b1e03a28d518cd223b21d7a
sk-t3ch <t3chflicks@gmail.com>
2021-04-12 21:02:13 +0100

    Fixed types

--- new_src/frontend/src/App.vue
+++ new_src/frontend/src/App.vue
@@ -1,19 +1,41 @@
 <template>
   <v-app>
-    <v-main>
-      <HelloWorld/>
+    <v-app-bar
+      app
+      color="black" 
+    >
+     <v-toolbar-title class="display-2 font-weight-bold secondary--text mr-4">CCTV OWL</v-toolbar-title>
+      <v-text-field
+        class="mt-7"
+        label="Owl URL"
+        dark
+        v-model="piUrl"
+        rounded
+        outlined
+        :append-icon="piUrlSet ? 'mdi-pencil' : 'mdi-check'"
+        @click:append="piUrlSet = !piUrlSet"
+        :readonly="piUrlSet"
+        placeholder="`http://raspberrypi.local:5000`"
+      >
+      </v-text-field>
+    </v-app-bar>
+    <v-main class="secondary">
+      <OwlView :pi-url="piUrl"/>
     </v-main>
+    <v-footer app color="black" class="font-weight-bold secondary--text"
+      >T3chFlicks</v-footer
+    >
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import OwlView from "./components/OwlView";
 
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
-    HelloWorld,
+    OwlView,
   },
 
   data: () => ({
