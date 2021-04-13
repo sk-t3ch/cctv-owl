acbd7d21c56fc0282cf7b4324993bd3e86f0ae13
T3ch Flicks <t3chflicks@gmail.com>
2021-04-13 11:35:57 +0100

    _

--- new_src/frontend/src/App.vue
+++ new_src/frontend/src/App.vue
@@ -2,25 +2,29 @@
   <v-app>
     <v-app-bar
       app
-      color="black" 
+      dark
     >
-     <v-toolbar-title class="display-2 font-weight-bold secondary--text mr-4">CCTV OWL</v-toolbar-title>
+     <v-toolbar-title class="font-weight-bold mr-4">CCTV OWL {{ $vuetify.breakpoint.name }}</v-toolbar-title>
       <v-text-field
         class="mt-7"
-        label="Owl URL"
+        label="Enter your CCTV OWL URL"
+        ref="addressbar"
         dark
+        dense
         v-model="piUrl"
         rounded
         outlined
         :append-icon="piUrlSet ? 'mdi-pencil' : 'mdi-check'"
         @click:append="piUrlSet = !piUrlSet"
+        @click="piUrlSet = false"
+        @keyup.enter="doThing"
         :readonly="piUrlSet"
-        placeholder="`http://raspberrypi.local:5000`"
+        placeholder="http://raspberrypi.local:5000"
       >
       </v-text-field>
     </v-app-bar>
     <v-main class="secondary">
-      <OwlView :pi-url="piUrl"/>
+      <OwlView :pi-url="piUrl" :pi-url-set="piUrlSet"/>
     </v-main>
     <v-footer app color="black" class="font-weight-bold secondary--text"
       >T3chFlicks</v-footer
@@ -35,11 +39,23 @@ export default {
   name: "App",
 
   components: {
-    OwlView,
+    OwlView
   },
-
-  data: () => ({
+  methods: {
+    doThing(){
+      this.piUrlSet = true;
+      this.$refs.addressbar.blur()
+    }
+  },
+  mounted() {
+    this.$refs.addressbar.focus()
+  },
+  data() {
+    return {
+    piUrlSet: false,
+    piUrl: null
     //
-  }),
+  }
+  },
 };
 </script>
--- new_src/frontend/src/components/OwlView.vue
+++ new_src/frontend/src/components/OwlView.vue
@@ -1,90 +1,89 @@
 <template>
-  <v-container fill-height fluid>
-    <v-row class="text-center">
-      <v-col class="mb-4">
-        <v-card class="secondary" dark>
-          <v-row justify="center">
-            <v-col cols="auto">
-              <v-card class="ma-2">
-                <v-card-title> Labels </v-card-title>
-                <v-card-text>
-                  <v-select
-                    :items="labels"
-                    v-model="config['label']"
-                  ></v-select>
-                </v-card-text>
+  <v-container>
+    <v-row class="my-2">
+      <v-col cols="12">
+        <v-card dark>
+        <v-row justify="center"> 
+          <v-col cols="12" xs="12" sm="12" md="4" lg="3" xl="2" align-self="center">
+            <v-card class="ma-2 elevation-12">
+              <v-card-title> Labels </v-card-title>
+              <v-card-text>
+                <v-select
+                  :items="labels"
+                  v-model="config['label']"
+                ></v-select>
+              </v-card-text>
+            </v-card>
+            <v-card class="ma-2  elevation-12">
+              <v-card-title> Certainty </v-card-title>
+              <v-card-text>
+                <v-slider
+                  v-model="config['threshold']"
+                  thumb-label="always"
+                  thumb-color="primary"
+                  max="100"
+                  min="0"
+                ></v-slider>
+              </v-card-text>
+            </v-card>
+          </v-col>
+          <v-col cols="12"  xs="12" sm="12" md="4" lg="6" xl="8" class="my-4">
+            <v-img
+              v-if="piUrlSet && piUrl"
+              :src="piUrl + '/video_feed'"
+              style="overflow: unset"
+              class="videoFeed"
+            >
+              <div class="left"></div>
+              <div class="right"></div>
+            </v-img>
+            <div v-else>
+              <v-card class="mx-4" min-height="40vh">
+                <tiny-owl />
               </v-card>
-              <v-card class="ma-2">
-                <v-card-title> Certainty </v-card-title>
-                <v-card-text>
-                  <v-slider
-                    v-model="config['threshold']"
-                    thumb-label="always"
-                    thumb-color="primary"
-                    max="100"
-                    min="0"
-                  ></v-slider>
-                </v-card-text>
-              </v-card>
-            </v-col>
-            <v-spacer />
-            <v-col cols="5" class="my-4">
-              <v-img
-                v-if="piUrlSet && piUrl"
-                :src="piUrl + '/video_feed'"
-                style="overflow: unset"
-              >
-                <div class="left"></div>
-                <div class="right"></div>
-              </v-img>
-              <v-img
-                v-else
-                style="overflow: unset"
-                :src="require('../assets/logo.jpg')"
-              >
-                <div class="left"></div>
-                <div class="right"></div>
-              </v-img>
+            </div>
+          </v-col>
+          <v-col cols="12"  xs="12" sm="12" md="4" lg="3" xl="2" align-self="center">
+            <v-card class="ma-2 elevation-12">
+              <v-card-title> Tracking </v-card-title>
+              <v-card-text>
+                <v-select
+                  :items="tracking"
+                  dark
+                  v-model="config['tracking']"
+                ></v-select>
+              </v-card-text>
+            </v-card>
 
-            </v-col>
-            <v-spacer />
-            <v-col cols="auto">
-              <v-card class="ma-2">
-                <v-card-title> Tracking </v-card-title>
-                <v-card-text>
-                  <v-select
-                    :items="tracking"
-                    dark
-                    v-model="config['tracking']"
-                  ></v-select>
-                </v-card-text>
-              </v-card>
-
-              <v-card class="ma-2">
-                <v-card-title> Actions </v-card-title>
-                <v-card-text>
-                  <v-checkbox 
-                    v-model="config['hoot']"
-                    label="Hoot"
-                  ></v-checkbox>
-                  <v-checkbox
-                    v-model="config['alert']"
-                    label="Alert"
-                  ></v-checkbox>
-                </v-card-text>
-              </v-card>
-            </v-col>
-          </v-row>
-        </v-card>
+            <v-card class="ma-2 elevation-12">
+              <v-card-title> Actions </v-card-title>
+              <v-card-text>
+                <v-checkbox 
+                  v-model="config['hoot']"
+                  label="Hoot"
+                ></v-checkbox>
+                <v-checkbox
+                  v-model="config['alert']"
+                  label="Alert"
+                ></v-checkbox>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-card>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
+import TinyOwl from '@/components/tinyOwl.vue'
 export default {
   name: "OwlView",
-  props: ['piUrl'],
+  props: ['piUrl', 'piUrlSet'],
+  components: {
+    TinyOwl
+  },
   data() {
     return {
       labels: ["person", "bird", "book", "dog", "all"],
@@ -95,8 +94,7 @@ export default {
         threshold: 70,
         hoot: false,
         alert: false,
-      },
-      piUrlSet: true
+      }
     };
   },
   watch: {
@@ -138,8 +136,8 @@ export default {
   position: absolute;
   content: "";
   background: black;
-  height: calc(110%);
-  width: calc(130%);
+  height: calc(105%);
+  width: calc(110%);
   border-radius: 25%;
   border: 3px solid white;
 }
@@ -148,5 +146,9 @@ export default {
 }
 .right:after {
   left: -15%;
+}
+
+.videoFeed {
+  /* width: 60%; */
 }
 </style>
