3a0a893ba3f6b84e7e68ffedc40133e79de4db6a
T3ch Flicks <t3chflicks@gmail.com>
2021-04-21 18:01:26 +0100

    _

--- src/app.py
+++ src/app.py
@@ -26,8 +26,8 @@ tracker = CentroidTracker(max_lost=0, tracker_output_format='mot_challenge')
 #                     tracker_output_format='mot_challenge')
 
 MOTOR_PIN = 12
-PI_IMAGE_WIDTH = 1280
-PI_IMAGE_HEIGHT = 960
+PI_IMAGE_WIDTH = 320
+PI_IMAGE_HEIGHT = 240
 OUTPUT_IMAGE_WIDTH = 640
 OUTPUT_IMAGE_HEIGHT = 480
 
